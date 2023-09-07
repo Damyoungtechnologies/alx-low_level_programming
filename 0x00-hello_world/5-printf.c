ad5db39ea06da3a464d8e4fc80ff6d5699956c87
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-07 16:38:23 -0700

    5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** 
+ * main - first entry point where compiler reads first
+ * Return: returns with the expression zero when successful
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
