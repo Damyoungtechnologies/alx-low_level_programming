7240795ed7571d25bb13489dcb7904b5f6030e3b
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-07 15:41:55 -0700

    yehaou

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+Be
+main - the entry point of tjr function
+Return - means the computer shouod terminate any other code following
+int main(){
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}	
