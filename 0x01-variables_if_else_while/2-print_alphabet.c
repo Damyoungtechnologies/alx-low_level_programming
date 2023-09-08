c3dae350fdd8ec1dec26d705b1000c9ab2663df3
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-08 16:02:41 -0700

    hiyia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - first entry
+* Return: 0 if successful
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
