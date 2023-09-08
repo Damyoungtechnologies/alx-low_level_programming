c41547d4d1d9c99da41b83438d0b3fb2aef9c56b
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-08 08:51:18 -0700

    call

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,11 @@
 *Return: zero when successful
 */
 int main(void)
-{   
-	printf("size of char: %lu bytes(s)\n",  	sizeof(char));
-	printf("size of an integer: %lu bytes(		s)\n", sizeof(int));
-	printf("size of a long integer: %lu byte	(s)\n", sizeof(long int));
-	printf("size of long int: %lu bytes(s)\n	", sizeof(long long int));
-	printf("size of a float:%lu bytes(s)\n",	sizeof(float));
+{
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("size of an integer: %lu byte(s)\n", sizeof(int));
+	printf("size of a long integer: %lu byte(s)\n", sizeof(long int));
+	printf("size of long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
