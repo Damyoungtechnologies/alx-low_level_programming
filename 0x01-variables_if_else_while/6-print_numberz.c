4fc8bd5d80cca9c58ec5f164f73aa39c3d823365
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-08 17:16:40 -0700

    yes yes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - first entry
+* Return: 0 when successful
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
