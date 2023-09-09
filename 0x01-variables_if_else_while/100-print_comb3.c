d270d02c0651858aaf50bb6f329fa0d01e892fd3
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-09 04:30:13 -0700

    telecommunication

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int i, a;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (a = 49; a <= 57; a++)
+		{
+			if (m > i)
+			{
+				putchar(i);
+				putchar(a);
+				if (i != 56 || a != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
