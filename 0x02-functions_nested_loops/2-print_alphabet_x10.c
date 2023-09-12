1ec4aefd6bfb3ccac5982825d2b6d8cea11e6b81
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-12 14:38:41 -0700

    alphabeth 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - fuction to display alphabeth 10 times
+* Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int k;
+	char b;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+
+		}
+	}
+	_putchar('\n');
+
+
+}
