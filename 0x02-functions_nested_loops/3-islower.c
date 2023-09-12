f094c78ee205547636155770b2fe6e37c846600c
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-12 16:01:27 -0700

    ascii code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - for lowercase check
+* Return: returns either 1 or 0
+* @c: parameter to be checked
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+	else
+		return (0);
+
+
+}
