cafbcb6f011afa387c086ecd0120a2a4cf483d76
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-12 17:00:47 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _abs - absolute value is positive
+* @a: parameter studied
+* Return: 0 or 1
+*/
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+
+
+
+}
