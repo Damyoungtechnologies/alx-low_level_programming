b7def0ea010da32f481850eb43b6732b52b4aa80
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-13 02:17:34 -0700

    e suffer me

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,23 @@ void times_table(void)
 	int i, j, result;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (j = 0; j < 10; j++)
 		{
 			result = i * j;
-			printf("%d", result);
-			_putchar(',');
+			if (j == 0)
+			{
+				printf("%d, ", result);
+			}
+			else
+			{
+			printf("%2d", result);
+			if (j != 9)
+				printf(", ");
+			}
 
 		}
 	printf("\n");
 
-
-
-
+	}
 }
