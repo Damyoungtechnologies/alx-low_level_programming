4eb5b6534255fa03bf003dda46aef3bc766b39a6
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-13 02:38:57 -0700

    shairman

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - to add two parameters
+* @a: first parameter
+* @b: second parameter
+* Return: add up the two parameters
+*/
+int add(int a, int b)
+
+{
+	return (a + b);
+}
