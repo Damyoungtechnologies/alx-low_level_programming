e6eddcf506c991babd33b1e12dc15fd2edd824a9
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-13 23:51:45 -0700

    mecho

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - first entry
+* Return: 0;
+*/
+int main(void)
+{
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int next;
+	int i;
+
+	printf("%lu, ", a);
+
+	for (i == 0; i < 50; i++)
+	{
+	printf("%lu", b);
+	next = a + b;
+	a = b;
+	b = next;
+	if (i != 49)
+		printf(", ");
+	}
+
+	printf("\n");
+	return (0);
+
+
+
+
+
+}
