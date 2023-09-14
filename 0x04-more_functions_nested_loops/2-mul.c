e61661036b1a9387f040e200f3481d66574db7ca
damyoungtechnologies <damyounggraphics@gmail.com>
2023-09-14 16:17:44 -0700

    shell

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - description
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+	return (0);
+
+}
