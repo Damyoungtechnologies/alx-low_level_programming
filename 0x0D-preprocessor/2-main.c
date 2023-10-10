b57701ff2bf374e13b68f5a181fe52e797284822
damyoungtechnologies <damyounggraphics@gmail.com>
2023-10-10 15:51:21 -0700

    huppy

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the first entry point to print the file
+ *
+ * Return: Always 0 (If Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
