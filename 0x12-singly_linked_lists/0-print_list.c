4dd8fb95ec8d39f26dca2736b7268a2d1cc006b3
damyoungtechnologies <damyounggraphics@gmail.com>
2023-10-20 10:07:22 -0700

    happy hour

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of linked list
@@ -13,10 +15,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-	if ((*h).str == NULL)
+	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
-	printf("[%u] %s\n", (*h).len, (*h).str);
+	printf("[%u] %s\n", h->len, h->str);
 
 	h = h->next;
 	cant++;
