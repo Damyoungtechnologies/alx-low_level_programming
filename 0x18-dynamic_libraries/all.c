53083ca56ae01e839a3f7ad2c4f7ac4d2d594e70
damyoungtechnologies <damyounggraphics@gmail.com>
2023-12-11 15:51:49 -0800

    hupp

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * add - add two integers
+ * &a: first int
+ * &b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**                                                     * mod - calculates the remainder of two integers       * &a: first int                                        * &b: second int                                       * Return: remainder from division                                     */                                     int mod(int a, int b)                                   
+{                                                              return (a % b);                                 
+}
+
+/**                                                     * sub - calculates the division of two integers       * &a: first int                                        * &b: second int                                       * Return: return th me calculate                                  
+*/                         
+int sub(int a, int b)                                     
+{                                                              return (a - b);                                 }
+
+/**                                                     * mul - calculates the remainder of two integers       * &a: first int                                        * &b: second int                                       * Return: addition                                     */                                                    int mul(int a, int b)                                      {                                                              return (a * b);                                 }
