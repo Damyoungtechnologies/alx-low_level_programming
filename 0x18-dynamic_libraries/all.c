97b9ff03b5055793fd9da2709eb995c915f2ad6b
damyoungtechnologies <damyounggraphics@gmail.com>
2023-12-13 01:54:19 -0800

    almost done

--- 0x18-dynamic_libraries/all.c
+++ 0x18-dynamic_libraries/all.c
@@ -20,3 +20,5 @@ int sub(int a, int b)
 {                                                              return (a - b);                                 }
 
 /**                                                     * mul - calculates the remainder of two integers       * &a: first int                                        * &b: second int                                       * Return: addition                                     */                                                    int mul(int a, int b)                                      {                                                              return (a * b);                                 }
+
+/**                                                     * div - calculates the remainder of two integers       * &a: first int                                        * &b: second int                                       * Return: division                                     */                                                    int div(int a, int b)                                      {                                                              return (a / b);                                 }
